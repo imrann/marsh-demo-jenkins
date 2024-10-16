a43a00d54e53dc138a4616d4d02c2546f13efea9
U1320165 <mohammad-imran.a.khan@mmc.com>
2024-10-16 15:17:08 +0530

    updated code againn

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -22,6 +22,6 @@ public class UserController {
             users.remove(user);
             return "User deleted: " + user;
         }
-        return "User not found";
+        return "User not found!!";
     }
 }
